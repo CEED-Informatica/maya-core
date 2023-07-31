59b1dd5a9f02dfccb4813e989dd86fe03c58738c
Alfredo Oltra <aoltra@gmail.com>
2023-07-31 15:27:44 +0000

    feat: modelo TaskMoodle
    Se encarga de definir aquellas tareas de moodle que están conectadas con Maya

--- models/__init__.py
+++ models/__init__.py
@@ -8,5 +8,6 @@ from . import res
 from . import employee
 from . import student
 from . import classroom
+from . import task_moodle
 from . import subject_student_rel
 from . import subject_classroom_rel
---
+++ models/task_moodle.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class TaskMoodle(models.Model):
+  """
+  Define una tarea de moodle que comunica con Maya. No permite que en una misma aula virtual
+  existan dos tareas con la misma key
+  """
+
+  _name = 'maya_core.task_moodle'
+  _description = 'Tarea de Moodle'
+
+  moodle_id = fields.Integer('Identificador Moodle', required = True)
+  key = fields.Char('Clave', required = True, help = 'Clave de búsqueda. Tiene que ser única para cada tarea y aula')
+  classroom_id = fields.Many2one('maya_core.classroom') 
+  description = fields.Char('Descripción de la tarea')
+  course_abbr = fields.Char(string = 'Ciclo', size = 5)
+  
+  _sql_constraints = [ 
+    ('unique_key', 'unique(key, classroom_id)', 'La clave de búsqueda tiene que ser única para cada tarea y aula.'),
+    ('unique_moodle_id', 'unique(moodle_id)', 'El identificador de moodle tiene que ser único.'),
+  ]
