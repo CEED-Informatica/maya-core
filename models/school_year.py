80f5d0cc84f7e253232790927249c17c5b58a885
Alfredo Oltra <aoltra@gmail.com>
2024-10-09 22:57:39 +0000

    fix: no se indicaba correctamente el id de la tarea para notificaciones

--- models/school_year.py
+++ models/school_year.py
@@ -1014,6 +1014,7 @@ class SchoolYear(models.Model):
         cron_template = self.env['maya_core.cron_register'].search([('key', '=', 'NOTV')])
         task_name = 'Notifica estado convalidaciones {} desde Aules {}'.format(course.abbr, 
               '/{}'.format(subject.year) if len(list(distinct_subject_tut)) > 1 else '')
+        job_data.task_id = classroom_id.get_task_id_by_key('validation')
         job_data.task2_id = classroom_id.get_task_id_by_key('validation_claim')
         task_data = self.cron_template2task(cron_template, task_name, str(job_data))
         task = (0, 0, task_data)
